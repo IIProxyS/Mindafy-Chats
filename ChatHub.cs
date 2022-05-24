09dfe99ca3ae7f0068e0e9b44a3b9f350b150ea7
Kevin Holguín <103459654+IIProxyS@users.noreply.github.com>
2022-05-24 08:26:01 -0500

    Add files via upload

---
+++ ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ApiChats
+{
+    public class ChatHub : Hub
+    {
+        public void Send(String user, String message)
+        {
+           Clients.All.SendMessage(user,message);
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MindafyApiChats.MvcApplication" Language="C#" %>
